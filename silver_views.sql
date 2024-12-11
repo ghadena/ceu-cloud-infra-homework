37dcaaaf5afa27660b75e9239ad37b253fb14377
ghadena <ghadena.h@gmail.com>
2024-12-11 22:49:51 +0100

    Update silver_views.sql

--- silver_views.sql
+++ silver_views.sql
@@ -1,2 +1,16 @@
--- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `silver_views` TABLE
- 
+DROP TABLE IF EXISTS ghadena_homework.silver_views;
+
+CREATE TABLE ghadena_homework.silver_views
+    WITH (
+          format = 'PARQUET',
+          parquet_compression = 'SNAPPY',
+          external_location = 's3://gh25-wikidata/datalake/views_silver/'
+    ) AS
+    SELECT 
+        article,
+        views, 
+        rank,
+        date
+    FROM ghadena_homework.bronze_views;
+
+select * from silver_views;

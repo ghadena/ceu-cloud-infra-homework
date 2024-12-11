7b13cac83d344828c9d91c68a5e036a6837ad527
ghadena <ghadena.h@gmail.com>
2024-12-11 22:50:32 +0100

    Update gold_allviews.sql

--- gold_allviews.sql
+++ gold_allviews.sql
@@ -1,2 +1,17 @@
--- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `gold_allviews` TABLE
- 
+DROP TABLE IF EXISTS ghadena_homework.gold_allviews;
+
+CREATE TABLE ghadena_homework.gold_allviews
+    WITH (
+          format = 'PARQUET',
+          parquet_compression = 'SNAPPY',
+          external_location = 's3://gh25-wikidata/datalake/gold_allviews/'
+    ) AS
+    SELECT 
+        article,
+        SUM(views) AS total_top_view,
+        MIN(rank) AS top_rank,
+        COUNT(DISTINCT date) AS ranked_days
+    FROM ghadena_homework.silver_views
+    GROUP BY article;
+    
+select * from gold_allviews;

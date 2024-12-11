f30864d56cbb179d5806af4b3bd7a1480745b393
ghadena <ghadena.h@gmail.com>
2024-12-11 22:49:13 +0100

    Update bronze_views.sql

--- bronze_views.sql
+++ bronze_views.sql
@@ -1,2 +1,16 @@
--- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `bronze_views` TABLE
+CREATE DATABASE ghadena_homework;
+
+DROP TABLE IF EXISTS ghadena_homework.bronze_views;
+
+CREATE EXTERNAL TABLE
+    bronze_views (
+    article STRING,
+    views INT, 
+    rank INT,
+    date DATE,
+    retrieved_at timestamp) 
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://gh25-wikidata/datalake/views/';
+
+select * from ghadena_homework.bronze_views;
  
